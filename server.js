081e32da8c3f59064bf00b5e13d622a7f4fedfc2
Cody Schwengler01~ <cschwengler@outlook.com>
2023-10-14 17:28:45 -0700

    created delete route

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
-const uuid =require('uuid');
+//using uuid to create a random number
+const { v4: uuidv4 } = require('uuid');
 
 //importing db.json
 const notesData = require('./db/db.json');
 
 //importing express
 const express = require('express');
+
 
 //importing built-in Node.ks package 'path to resolve path of files that located on the server
 const path = require('path');
@@ -20,6 +22,7 @@ const PORT = 3001;
 
 //static middleware point to the public folder
 app.use(express.static('public'));
+app.use(express.json());
 
 //standard get to display index.html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
@@ -32,20 +35,32 @@ app.get('/notes', (req,res) =>
 
 // Handle GET request to retrieve notes
 app.get('/api/notes', (req, res) => {
-    console.log(`${req.method} request has been received`);
     return res.json(notesData);
   });
   
   // Handle POST request to create a new note
   app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
     console.log(req.body);
-    const { title, text } = req.body;
-    const newNote = {title, text, id: uuid()};
+    // Generate a unique ID using uuid
+    newNote.id = uuidv4();
     notesData.push(newNote);
-    // Save the updated notes to db.json
     fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
     return res.json(newNote);
   });
+  
+  // delete route
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  // Find the index of the note with the provided ID
+  const index = notesData.findIndex((note) => note.id === noteId);
+  if (index !== -1) {
+    // Remove the note from the array
+    notesData.splice(index, 1);
+    // Save the updated notes to db.json
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+  }
+});
 //how to get port to listen and to use server
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
